01bf4729e48a11678053678b71b0430d0ba578ef
Hyunji <chj09255@gmail.com>
2023-05-10 06:02:17 +0900

    yyy

--- WIN32_Framework/WIN32_Framework/소스.cpp
+++ WIN32_Framework/WIN32_Framework/소스.cpp
@@ -771,3 +771,76 @@ void SceneManager::Destroy()
 	SceneState = nullptr;
 }
 */
+
+// InputManager.h
+/*
+#pragma once
+#include "Include.h"
+
+class InputManager
+{
+private:
+	static InputManager* Instance;
+public:
+	static InputManager* GetInstance()
+	{
+		if (Instance == nullptr)
+			Instance = new InputManager;
+		return Instance;
+	}
+private:
+	DWORD inputKey;
+public:
+	DWORD GetKey() {	return inputKey; }
+
+	void CheckKey();
+private:
+	InputManager();
+public:
+	~InputManager();
+};
+*/
+
+// InputManager.cpp
+/*
+#include "InputManager.h"
+
+InputManager* InputManager::Instance= nullptr;
+
+InputManager::InputManager()
+{
+}
+
+InputManager::~InputManager()
+{
+}
+
+void InputManager::CheckKey()
+{
+	inputKey = 0;
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+		inputKey |= KEYID_UP;
+
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+		inputKey |= KEYID_DOWN;
+
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+		inputKey |= KEYID_LEFT;
+
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+		inputKey |= KEYID_RIGHT;
+
+	if (GetAsyncKeyState(VK_RETURN) & 0x8000)
+		inputKey |= KEYID_RETURN;
+
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+		inputKey |= KEYID_SPACE;
+
+	if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
+		inputKey |= KEYID_ESCAPE;
+
+	if (GetAsyncKeyState(VK_CONTROL) & 0x8000)
+		inputKey |= KEYID_CONTROL;
+}
+*/

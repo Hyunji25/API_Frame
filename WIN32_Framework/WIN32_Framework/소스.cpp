755bc812613da7da543742ed08309ccd29775e7b
admin <chj09255@gmail.com>
2023-05-12 09:02:49 +0900

    t

--- WIN32_Framework/WIN32_Framework/소스.cpp
+++ WIN32_Framework/WIN32_Framework/소스.cpp
@@ -844,3 +844,88 @@ void InputManager::CheckKey()
 		inputKey |= KEYID_CONTROL;
 }
 */
+
+// ObjectManager.h
+/*
+#include "Include.h"
+
+class GameObject;
+class ObjectManager
+{
+private:
+	static ObjectManager* Instance;
+public:
+	static ObjectManager* GetInstance()
+	{
+		if (Instance == nullptr)
+			Instance = new ObjectManager;
+		return Instance;
+	}
+private:
+	map<string, list<GameObject*>> ObjectList;
+public:
+	void AddObject(GameObject* _Object);
+
+	list<GameObject*>* GetObjectList(string key);
+private:
+	ObjectManager();
+public:
+	~ObjectManager();
+};
+*/
+
+// ObjectManager.cpp
+/*
+#include "ObjectManager.h"
+#include "GameObject.h"
+
+//ObjectManager* ObjectManager::Instance = nullptr;
+
+ObjectManager::ObjectManager()
+{
+
+}
+
+ObjectManager::~ObjectManager()
+{
+
+}
+
+void ObjectManager::AddObject(GameObject* _Object)
+{
+	// 파라메터가 가르키고 있는 키 값이 현재 map에 포함되어 있는지 확인
+	map<string, list<GameObject*>>::iterator iter = ObjectList.find(_Object->GetKey());
+
+	// 만약 키가 없다면...
+	if (iter == ObjectList.end())
+	{
+		// map에 포함시킬 list를 생성한 후...
+		list<GameObject*> tempList;
+
+		// 리스트에 오브젝트를 추가
+		tempList.push_back(_Object);
+
+		// 오브젝트가 추가된 리스트를 map에 추가
+		ObjectList.insert(make_pair(_Object->GetKey(), tempList));
+	}
+	// 이미 키가 존재한다면...
+	else
+		// 해당 리스트에 오브젝트를 추가
+		iter->second.push_back(_Object);
+}
+
+list<GameObject*>* ObjectManager::GetObjectList(string key)
+{
+	// 키 값이 현재 map에 포함되어 있는지 확인
+	map<string, list<GameObject*>>::iterator iter = ObjectList.find(key);
+
+	// 만약 키가 없다면...
+	if (iter == ObjectList.end())
+		// nullptr을 반환
+		return nullptr;
+	// 이미 키가 존재한다면...
+	else
+		// second = value = list<GameObject*>를 반환
+		return &iter->second;
+}
+*/
